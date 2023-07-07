60cb7014c6016e47799afc5f6d98536572545d0a
Boca75 <arthur@uvnet.fr>
2023-07-07 18:35:50 +0200

    update login

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,48 +1,54 @@
-const url = "http://localhost:5678/api/users/login";
-const errorMessage = document.getElementById("errorMessage");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const submit = document.getElementById("submit");
-
-// Vérification et stockage du jeton retourné par l'API - redirection - message d'erreur
-const handleResponse = async (response) => {
-    if (response.ok) {
-        const userData = await response.json();
-        console.log(userData);
-        if (userData) {
-            window.sessionStorage.setItem("userData", JSON.stringify(userData));
-            window.sessionStorage.setItem("token", userData.token);
-            window.location.replace("./index.html");
+document.addEventListener('DOMContentLoaded', () => {
+    // Récupération des éléments du formulaire
+    const emailInput = document.getElementById('email')
+    const passwordInput = document.getElementById('password')
+    const submitButton = document.getElementById('submit')
+    const errorFormMessage = document.getElementById('errorFormMessage')
+  
+    // J'ajoute l'événement de soumission du formulaire
+    submitButton.addEventListener('click', async (event) => {
+      event.preventDefault() // Empêche la soumission du formulaire par défaut
+  
+      const email = emailInput.value
+      const password = passwordInput.value
+  
+      // Vérification des champs du formulaire
+      if (email === '' || password === '') {
+        errorFormMessage.textContent = 'Erreur email et mot de passe .'
+        return
+      }
+  
+      // J'effectue la requête de connexion à l'API avec la méthode POST
+      const url = 'http://localhost:5678/api/users/login'
+      const data = {
+        email: email,
+        password: password,
+      }
+  
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+  
+        if (response.ok) {
+          const userData = await response.json()
+          console.log(userData)
+          if (userData) {
+            window.sessionStorage.setItem('userData', JSON.stringify(userData))
+            window.sessionStorage.setItem('token', userData.token)
+            window.location.replace('./index.html')
+          }
+        } else {
+          throw new Error('Erreur de connexion.')
         }
-    } else {
-        errorMessage.innerHTML = "Votre email et/ou mot de passe ne correspondent pas";
-    }
-};
-
-
-// Stockage des informations d'identification de l'utilisateur - requête HTTP POST - Appel de la fonction handleResponse() pour traiter la réponse.
-const fetchHandler = async (event) => {
-    event.preventDefault();
-    const user = {
-        email: email.value,
-        password: password.value,
-    };
-    const options = {
-        method: "POST",
-        body: JSON.stringify(user),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
-    try {
-        const response = await fetch(url, options);
-        handleResponse(response);
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-// Ajout d'un écouteur d'évènement sur le bouton de soumission du formulaire - Appel de la fonction fetchHandler() lorsque l'utilisateur clique sur le bouton.
-submit.addEventListener("click", fetchHandler);
-
-
+      } catch (error) {
+        errorFormMessage.textContent = error.message
+      }
+    })
+  })
+  
+  
